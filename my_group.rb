b287eb51010630b111541cb2f42e31e1c31f2481
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-14 17:03:37 +0100

    second try, updating my_group with prope array

--- my_group.rb
+++ my_group.rb
@@ -1,10 +1,5 @@
 name = "my_group"
-names = ["Karro is a 34 year old woman", "Eric is a 19 year old man", "Steve is a 28 year old man"]
-collection = ["person_1", "person_2", "person_3"]
-person_1 = {name: "Karro", gender: "Female", age: 34}
-person_2 = {name: "Eric", gender: "Male", age: 19}
-person_3 = {name: "Steve", gender: "Male", age: 28}
-
-names.each do |name|
-    puts "#{name}"
-    end
+    collection = ["Karro", "Robert", "Angela"]
+        person_1 = {name: "Karro", gender: "female", age: 34}
+        person_2 = {name: "Robert", gender: "male", age: 19}
+        person_3 = {name: "Angela", gender: "female", age: 56}
