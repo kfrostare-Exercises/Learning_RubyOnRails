afded996912be6bbf7e5a2f29cafcd93a8727121
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-14 13:47:04 +0100

    cheat iteration added to array

--- my_group.rb
+++ my_group.rb
@@ -1,9 +1,10 @@
 name = "my_group"
+names = ["Karro is a 34 year old woman", "Eric is a 19 year old man", "Steve is a 28 year old man"]
 collection = ["person_1", "person_2", "person_3"]
 person_1 = {name: "Karro", gender: "Female", age: 34}
 person_2 = {name: "Eric", gender: "Male", age: 19}
 person_3 = {name: "Steve", gender: "Male", age: 28}
 
-
-
-puts collection
+names.each do |name|
+    puts "#{name}"
+    end
