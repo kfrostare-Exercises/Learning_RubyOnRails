b66aae126a2a4cfce57df582d83c84f4d774b9ab
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-14 11:51:49 +0100

    my_group added

---
+++ my_group.rb
@@ -0,0 +1,9 @@
+name = "my_group"
+collection = ["person_1", "person_2", "person_3"]
+person_1 = {name: "Karro", gender: "Female", age: 34}
+person_2 = {name: "Eric", gender: "Male", age: 19}
+person_3 = {name: "Steve", gender: "Male", age: 28}
+
+
+
+puts collection
