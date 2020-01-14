f4f17aab21c52050523aea7ab4f05fbf80c261d6
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-14 20:35:45 +0100

    completed the age.rb challenge

---
+++ age.rb
@@ -0,0 +1,39 @@
+
+num2 = 10
+num3 = 20
+num4 = 30
+num5 = 40
+
+
+puts ("")
+puts ("HEY DUDE") 
+puts ("Enter your age and I'll tell you your future!")
+puts ("")
+puts ("")
+num1 = gets.chomp().to_i
+puts ("")
+puts ("")
+puts ("Wait, what?")
+puts ("")
+puts ("")
+puts ("Ok so like...")
+puts ("did you know that in #{num2} years you'll be #{num1 + num2} ?")
+puts ""
+puts ("")
+puts ("...and in #{num3} years you'll be #{num1 + num3}... #{num1 + num3} DUDE!!!")
+puts ("")
+puts ("")
+puts ("I'm just sayin'... #{num1} is prett old, you'll be #{num1 + num4} like almost tomorrow.")
+puts ("")
+puts ("")
+puts ("What's the poing in trying to tell your future.. you are CRAZY old, man! In #{num5} years you'll be like #{num1 + num5} so yeah pretty much dead or whatever... .")
+puts ("Look all I can tell you is get off your computer and go LIVE whatever life you have left...")
+puts ("")
+puts ("")
+puts ("")
+puts ("")
+puts ("... What are you waiting for - GO, GO, GOO!!!")
+
+
+
+
