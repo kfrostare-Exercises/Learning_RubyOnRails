b6adb803a2ce08204188c5a5a5e2ad4332ae857b
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-14 18:14:19 +0100

    add movie list from hashes

---
+++ movie_list.rb
@@ -0,0 +1,12 @@
+
+movie1 = {name: "The Wizard of Oz", year: "1939"}
+movie2 = {name: "The Irishman", year: "2019"}
+movie3 = {name: "A Star Is Born", year: "2018"}
+movie4 = {name: "Modern Times", year: "1936"}
+movie5 = {name: "Black Panther", year: "2018"}
+
+puts " #{movie1[:name]}"
+puts " #{movie2[:name]}"
+puts " #{movie3[:name]}"
+puts " #{movie4[:name]}"
+puts " #{movie5[:name]}"
