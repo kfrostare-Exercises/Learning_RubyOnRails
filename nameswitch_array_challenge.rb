9f5133e4ec11bed61818ecdd2f32397d915c586d
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-15 09:31:36 +0100

    add namechange_array_challenge file

---
+++ nameswitch_array_challenge.rb
@@ -0,0 +1,6 @@
+# Challenge: You run the following code and get the following error message, what is the problem and how can it be fixed?
+## Answer: To change a name you run the number in which that object occurs in the array not the name itself
+
+names = ["bob", "joe", "susan", "margaret"]
+puts names[3] = "jody"
+puts names
