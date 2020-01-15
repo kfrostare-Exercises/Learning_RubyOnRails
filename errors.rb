5a1bdfce9e2aa444b3f447f4cc82d682494797be
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-15 12:13:36 +0100

    add hashes_valuesboolean_rb file

---
+++ errors.rb
@@ -0,0 +1,8 @@
+
+person = {name: "Bob", occupation: "web developer", hobbies: "painting"}
+
+
+
+puts person.fetch(:name)
+puts
+puts person.has_value?("Fran")
---
+++ hashes_value_boolean.rb
@@ -0,0 +1,5 @@
+# What method could you use to find out if a Hash contains a specific value in it? Write a program to demonstrate this use.
+person = {name: "Bob", occupation: "web developer", hobbies: "painting"}
+
+
+puts person.has_value?("Fran")
