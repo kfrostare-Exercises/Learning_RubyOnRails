3f13e94b0942fcbb64867a262d7467be58c1d3f3
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-15 10:12:02 +0100

    add hashes_family_challenge.rb

---
+++ hashes_family_challenge.rb
@@ -0,0 +1,10 @@
+# Given
+
+family = {
+    uncles: ["bob", "joe", "steve"],
+    sisters: ["jane", "jill", "beth"],
+    brothers: ["frank", "rob", "david"],
+    aunt: ["mary", "sally", "susan"]
+}
+
+puts family[:sisters] + family[:brothers]
