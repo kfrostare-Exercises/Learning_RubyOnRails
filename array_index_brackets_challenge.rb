c6257fd83e31b5e06f3be9c1aca336369a809a29
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-15 09:01:48 +0100

    add array index brackets challenge

---
+++ array_index_brackets_challenge.rb
@@ -0,0 +1,8 @@
+# Exersice one, array index five returns the third number on the list
+# Exersice two, returns an undefined message probably because of the square brackets together with the .index method
+# Exersice three, array index five returns the sixth (5th) number on the list
+
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+puts arr[5]
